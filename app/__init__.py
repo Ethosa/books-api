1013793df8e2fb1eacffc607beb4c3a8a7615fe7
Ethosa <social.ethosa@gmail.com>
2024-12-17 14:38:55 +0700

    fix: cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from mounts.user import user_app
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_headers="*",
+    allow_methods="*",
+    allow_origins="*",
+    allow_credentials=True
+)
 
 
 app.mount('/user', user_app)
